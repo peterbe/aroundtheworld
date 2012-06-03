7cd0118f57fc74abc41abe27838cb7c87c07b8fa
Peter Bengtsson <mail@peterbe.com>
2012-06-03 16:50:28 -0700

    local_settings.py must have a cookie secret

--- server/settings.py
+++ server/settings.py
@@ -5,7 +5,6 @@ path = lambda *a: op.join(ROOT, *a)
 PROJECT_TITLE = u"Around The World"
 DATABASE_NAME = "aroundtheworld"
 
-COOKIE_SECRET = "12orTzK2XqaGeYdkL3gmUejIFuY37EQn92XsTo2v/Vi="
 TWITTER_CONSUMER_KEY = None
 TWITTER_CONSUMER_SECRET = None
 
@@ -23,11 +22,13 @@ THUMBNAIL_DIRECTORY = path('static/thumbnails')
 QUIZ_MIN_NO_QUESTIONS = 10
 PINPOINT_NO_QUESTIONS = 10
 
+COOKIE_SECRET = ""
+
 try:
     from local_settings import *
 except ImportError:
     pass
-
+assert COOKIE_SECRET, "It must be set in local_settings.py"
 
 #assert TWITTER_CONSUMER_KEY
 #assert TWITTER_CONSUMER_SECRET
